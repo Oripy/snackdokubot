728a9007cf4f38a0c1a55658d8f7079e4ec06c7a
Pierre Maurier <pierre.maurier@gadz.org>
2025-05-26 09:02:26 +0200

    More generic sheet manipulations

--- sheet_tools.py
+++ sheet_tools.py
@@ -11,8 +11,7 @@ from google.oauth2 import service_account
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 SPREADSHEET_ID = config['DEFAULT']["SHEET_ID"]
-SHEET_NAME = 'Sheet1'
-DATA_RANGE = f'{SHEET_NAME}!A1:I1'
+SHEET_NAME = ['Sheet1', 'ExtraBite']
 
 try:
     secret_file = os.path.join(os.getcwd(), 'client_secret.json')
@@ -23,10 +22,10 @@ try:
 except OSError as e:
     print(e)
 
-def edit_line(message_id, date, title, author, edit_link, solve_link, grn, yello, red):
+def edit_line(message_id, date, title, author, edit_link, solve_link, reactions, sheet_number=0):
     message_ids = []
     try:
-        message_ids = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=f'{SHEET_NAME}!A1:A', majorDimension='COLUMNS').execute()['values'][0]
+        message_ids = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=f'{SHEET_NAME[sheet_number]}!A1:A', majorDimension='COLUMNS').execute()['values'][0]
     except KeyError:
         pass
 
@@ -37,32 +36,32 @@ def edit_line(message_id, date, title, author, edit_link, solve_link, grn, yello
         pass
 
     values = [
-        [str(message_id), date, title, author, edit_link, solve_link, grn, yello, red],
+        [str(message_id), date, title, author, edit_link, solve_link, *reactions],
     ]
 
     data = { 'values': values }
     sheet.values().update(spreadsheetId=SPREADSHEET_ID,
                           body=data,
-                          range=f'{SHEET_NAME}!A{index+1}:I{index+1}',
+                          range=f'{SHEET_NAME[sheet_number]}!A{index+1}:{chr(65+len(values)-1)}{index+1}',
                           valueInputOption='USER_ENTERED').execute()
 
-def get_line(message_id):
+def get_line(message_id, sheet_number=0):
     message_ids = []
     try:
-        message_ids = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=f'{SHEET_NAME}!A1:A', majorDimension='COLUMNS').execute()['values'][0]
+        message_ids = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=f'{SHEET_NAME[sheet_number]}!A1:A', majorDimension='COLUMNS').execute()['values'][0]
     except KeyError:
         pass
 
     if str(message_id) in message_ids:
         index = message_ids.index(str(message_id))
-        return sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=f'{SHEET_NAME}!B{index+1}:I{index+1}').execute()['values'][0]
+        return sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=f'{SHEET_NAME[sheet_number]}!B{index+1}:F{index+1}').execute()['values'][0]
     else:
         return None
 
-def del_line(message_id):
+def del_line(message_id, sheet_number=0):
     message_ids = []
     try:
-        message_ids = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=f'{SHEET_NAME}!A1:A', majorDimension='COLUMNS').execute()['values'][0]
+        message_ids = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=f'{SHEET_NAME[sheet_number]}!A1:A', majorDimension='COLUMNS').execute()['values'][0]
     except KeyError:
         pass
 
